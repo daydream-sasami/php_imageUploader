3f7a5ce6e9cc91a95d244a50022d393307898db0
Makoto Masuda <minoto712545@gmail.com>
2019-08-30 00:56:52 +0900

    add GD?

--- www/html/index.php
+++ www/html/index.php
@@ -1,5 +1,21 @@
 <?php
 
+ini_set('display_errors', 1);
+define('MAX_FILE_SIZE', 1 * 1024 * 1024); //1MB
+define('THUMBNAIL_WIDTH', 400);
+define('IMAGES_DIR', __DIR__ . '/images');
+define('THUMBNAIL_DIR', __DIR__ . '/thumbs');
+
+// if (!function_exists('imagecreatetruecolor')) {
+//   echo 'GD not installed';
+//   exit;
+// }
+
+function h($s) {
+  return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+  exit;
+}
+
 ?>
 
 
@@ -18,7 +34,7 @@
 </head>
 <body>
   <form action="" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="MAX_FILE_SIZE" value="">
+    <input type="hidden" name="MAX_FILE_SIZE" value="<?php h(MAX_FILE_SIZE); ?>">
     <input type="file" name="image">
     <input type="submit" value="upload">
   </form>
